794180369d2e50534f462b2bde523b0006d893e0
name <email@example.com>
2020-07-29 22:42:20 +0800

    Multiplcation

---
+++ Multiplcation.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a,b;
+	int c;
+	
+	cout << "This is a multiplcation program\n";
+	
+	cout << "Push a into this program\n";
+	cin >> a;
+	
+	cout << "Push b into this program\n";
+	cin >> b;
+	
+	c=a*b;
+	
+	cout << c;
+	
+	return 0;
+}
