e47b968d3e0c9fde26e2566c686723136061ffbb
name <email@example.com>
2020-07-29 17:11:00 +0800

    1

---
+++ helloworld.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+ int a,b;
+ int c;
+ 
+ cout << "a+b=c" << endl;
+ 
+ cout << "a" << endl;
+ cin >> a;
+ cout << "b" << endl;
+ cin >> b;
+ 
+ c=a+b;
+ 
+ cout << c << endl;
+ 
+ return 0;
+}
